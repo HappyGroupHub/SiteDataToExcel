1365375b41dfe53db782d01c7db72c3f4748bc16
LD <chiyc88@gmail.com>
2024-02-18 00:04:07 +0800

    Update - use requests instead of selenium since the website has become static

--- app.py
+++ app.py
@@ -1,53 +1,24 @@
-import concurrent.futures
-
 import openpyxl
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.support.ui import WebDriverWait
-
-
-def driver_get_text(driver, locator):
-    """Get text of element.
-
-    :param driver: WebDriver instance.
-    :param locator: Locator of element.
-    :return: Text of element.
-    """
-    return WebDriverWait(driver, 10).until(ec.presence_of_element_located(locator)).text
+import requests
+from bs4 import BeautifulSoup
 
 
 def get_data_from_url(url):
-    driver = webdriver.Chrome()  # Replace with your preferred browser driver
-    driver.get(url)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
 
-    # Wait for the table to load (adjust timeout as needed)
-    wait_time = 10  # seconds
-    driver.implicitly_wait(wait_time)
+    # Find the specific table with ID "spectable"
+    table = soup.find("table", id="spectable")
 
-    data = []
-    for i in range(1, 11):  # Get column headers
-        data.append(driver_get_text(driver, (
-            By.XPATH, f"/html/body/div/div/div/table/thead/tr[2]/td[{i}]")))
+    if not table:
+        print("Error: Table with ID 'spectable' not found.")
+        return None
 
-    # Get table data
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        rows_data = list(
-            executor.map(lambda row_num: get_data_from_row(driver, row_num), range(2, 5002)))
-    data.extend(rows_data)
+    # Extract data from table rows within the "thead" section
+    rows = table.find("thead").find_all("tr")
+    data = [[cell.text.strip() for cell in row.find_all("td")] for row in rows]
 
-    driver.quit()  # Close the browser after data extraction
-    print(data)
     return data
-
-
-def get_data_from_row(driver, row_num):
-    temp = []
-    for j in range(1, 11):
-        temp.append(driver_get_text(driver, (
-            By.XPATH, f"/html/body/div/div/div/table/thead/tr[{row_num}]/td[{j}]")))
-    print(f"{row_num} done")
-    return temp
 
 
 def save_data_to_excel(data, filename):
@@ -72,4 +43,4 @@ if __name__ == "__main__":
         save_data_to_excel(data, "enterprise_visited.xlsx")
         print("Data saved successfully to enterprise_visited.xlsx")
     else:
-        print("Error: Data extraction failed. No matching data found.")
+        print("Error: Data extraction failed.")
